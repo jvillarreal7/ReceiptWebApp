5e4cf1e595f868e5383b982864b3404f9d995a5b
Jaime Villarreal <jvillarreal337@gmail.com>
2019-05-13 17:00:02 -0500

    Add project files.

---
+++ ReceiptWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReceiptWebApp.MvcApplication" Language="C#" %>
---
+++ ReceiptWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReceiptWebApp.Startup))]
+namespace ReceiptWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
