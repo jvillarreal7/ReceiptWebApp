020d7acdcbf65cfc4d449646e9a7b9391f97e703
Jaime Villarreal <jvillarreal337@gmail.com>
2019-05-13 18:26:35 -0500

    Formulario para crear recibo.

---
+++ ReceiptWebApp/Controllers/ReceiptsController.cs
@@ -0,0 +1,28 @@
+﻿using ReceiptWebApp.Models;
+using ReceiptWebApp.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ReceiptWebApp.Controllers
+{
+    public class ReceiptsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReceiptsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new ReceiptFormViewModel
+            {
+                Providers = _context.Providers.ToList(),
+                CurrencyTypes = _context.CurrencyTypes.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ReceiptWebApp/ViewModels/ReceiptFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReceiptWebApp.Models;
+using System.Collections.Generic;
+
+namespace ReceiptWebApp.ViewModels
+{
+    public class ReceiptFormViewModel
+    {
+        public string Amount { get; set; }
+        public string Comments { get; set; }
+        public string CreatedDate { get; set; }
+
+        public int Provider { get; set; }
+        public IEnumerable<Provider> Providers { get; set; }
+        public int CurrencyType { get; set; }
+        public IEnumerable<CurrencyType> CurrencyTypes { get; set; }
+    }
+}
